4d5826b0c6989a60ba02fc9187e334b4f2c344e7
eorodrig <eorodrig@ualberta.ca>
2016-01-26 23:53:34 -0700

    changed contact form to its own html page (for simplicity)

--- js/loadMaster.js
+++ js/loadMaster.js
@@ -1,2 +1,11 @@
-  $("#pageMenu").load("MasterPage.html #mainMenu");
-  $("#pageFooter").load("MasterPage.html #mainFooter");
+	//This loads the master page elements
+	$("#pageMenu").load("MasterPage.html #mainMenu");
+	$("#pageFooter").load("MasterPage.html #mainFooter");
+
+  
+	//This checks for contact form
+	var cform = document.getElementById('contactForm');
+	if (cform =! null){
+		$("#contactForm").load("ContactForm.html #form");
+	
+	}
